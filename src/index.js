af449aada95accc42101aa8b93f292af352225be
Lxr-Dev <53893016+Lxr-Dev@users.noreply.github.com>
2021-12-11 02:01:04 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import app from "./app";
 import { PORT } from "./config";
+import { MONGODB_URI } from "./config";
 
 // database
 import "./config/mongoose";
 
 // Starting the server
-app.listen(PORT);
+app.listen(PORT, MONGODB_URI);
 console.log("Server on port", app.get("port"));
