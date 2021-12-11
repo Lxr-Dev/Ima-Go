98ce73d8459eb98bc3ec17b73ec61905f201890e
Lxr-Dev <53893016+Lxr-Dev@users.noreply.github.com>
2021-12-11 03:49:29 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 import { config } from "dotenv";
 config();
 
-export const MONGODB_URI = process.env.MONGODB_URI || "mongodb+srv://admin:admin@cluster0.0zuzt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+export const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/ima-go";
 export const PORT = process.env.PORT || 3000;
